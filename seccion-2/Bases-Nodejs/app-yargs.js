0044aceff9f905b1e8631d319254b2684bb9ee82
Alvaro Vargas <alvaro.vargas@axity.com>
2019-04-11 13:06:38 -0400

    yargs

---
+++ seccion-2/Bases-Nodejs/app-yargs.js
@@ -0,0 +1,20 @@
+const {crearArchivo, listarTabla} = require('./multiplicar/multiplicar')
+const argv = require('config/yargs').argv;
+
+let comando = argv._[0];
+
+switch (comando) {
+  case 'listar':
+    listarTabla(argv.base, argv.limite);
+    break;
+  case 'crear':
+    crearArchivo(argv.base, argv.limite)
+      .then(archivo => console.log(`Archivo creado: ${archivo} `))
+      .catch(err => console.log(err));
+    break;
+  default:
+    console.log('Comando no reconocido')
+}
+
+let argv2 = process.argv;
+//console.log('Limite:' + argv.limite)
--- seccion-2/Bases-Nodejs/multiplicar/multiplicar.js
+++ seccion-2/Bases-Nodejs/multiplicar/multiplicar.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
 
 
-crearArchivo = (base) => {
+crearArchivo = (base, limite) => {
   return new Promise((resolve, reject) => {
     if (!Number(base)) {
       reject(`El valor ingresado ${base}, no es un número`);
       return
     }
+    if (!Number(limite)) {
+      reject(`El valor ingresado ${limite}, no es un número`);
+      return
+    }
     let data = '';
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= limite; i++) {
       data += `${base} * ${i} = ${i * base} \n`
     }
 
-    fs.writeFile(`tablas/Tabla-del-${base}.txt`, data, (err) => {
+    fs.writeFile(`tablas/Tabla-del-${base} x ${limite}.txt`, data, (err) => {
       if (err)
         reject(err);
       else
-        resolve(`tabla-del-${base}.txt`);
+        resolve(`tabla-del-${base} x ${limite}.txt`);
     });
   })
 };
 
+listarTabla = (base, limite = 10) => {
+
+  for (let i = 1; i <= limite; i++) {
+    console.log(`${i} * ${base} = ${i * base}`);
+  }
+
+};
+
 module.exports = {
-  crearArchivo
+  crearArchivo,
+  listarTabla
 };
